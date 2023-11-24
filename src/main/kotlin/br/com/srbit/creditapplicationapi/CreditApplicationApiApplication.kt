49279233ac83cb229866dab4c2364c59e7c5ea1a
Lucas Ilussencio da Silva <ilussencio@gmail.com>
2023-11-24 11:40:01 -0300

    Criação do projeto

---
+++ src/main/kotlin/br/com/srbit/creditapplicationapi/CreditApplicationApiApplication.kt
@@ -0,0 +1,11 @@
+package br.com.srbit.creditapplicationapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditApplicationApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreditApplicationApiApplication>(*args)
+}
