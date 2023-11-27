6ff2a20594f77b9b63e55c7991a09ffa39f8d50d
Lucas Ilussencio da Silva <ilussencio@gmail.com>
2023-11-27 07:16:47 -0300

    Criando camada de persistencia

---
+++ src/main/kotlin/br/com/srbit/creditapplicationapi/service/impl/CreditService.kt
@@ -0,0 +1,30 @@
+package br.com.srbit.creditapplicationapi.service.impl
+
+import br.com.srbit.creditapplicationapi.model.Credit
+import br.com.srbit.creditapplicationapi.repositories.CreditRepository
+import br.com.srbit.creditapplicationapi.service.ICreditService
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class CreditService(
+    private val creditRepository: CreditRepository,
+    private val customerService: CustomerService
+): ICreditService {
+    override fun save(credit: Credit): Credit {
+        credit.apply {
+            customer = customerService.findById(credit.customer?.id!!)
+        }
+        return this.creditRepository.save(credit)
+    }
+
+    override fun findAllByCustomer(customerId: Long): List<Credit> = this.creditRepository.findAllByCredits(customerId)
+
+    override fun findByCreditCode(customerId: Long, creditCode: UUID): Credit {
+        val credit: Credit = this.creditRepository.findByCreditCode(creditCode)
+            ?: throw RuntimeException("creditCode não encontrado")
+        return if(credit.customer?.id == customerId) credit else throw RuntimeException("Contact admin")
+
+    }
+
+}
---
+++ src/main/kotlin/br/com/srbit/creditapplicationapi/service/impl/CustomerService.kt
@@ -0,0 +1,21 @@
+package br.com.srbit.creditapplicationapi.service.impl
+
+import br.com.srbit.creditapplicationapi.model.Customer
+import br.com.srbit.creditapplicationapi.repositories.CustomerRepository
+import org.springframework.stereotype.Service
+import java.lang.RuntimeException
+import br.com.srbit.creditapplicationapi.service.ICustomerService as ICustomerService1
+
+@Service
+class CustomerService(
+    private val customerRepository: CustomerRepository
+): ICustomerService1 {
+    override fun save(customer: Customer): Customer {
+        return this.customerRepository.save(customer)
+    }
+
+    override fun findById(id: Long): Customer = this.customerRepository.findById(id).orElseThrow(){
+            throw RuntimeException("Id $id não encontrado")
+    }
+    override fun delete(id: Long) = this.customerRepository.deleteById(id)
+}
