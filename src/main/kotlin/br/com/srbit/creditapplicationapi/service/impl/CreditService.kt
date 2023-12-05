6726e2a719fba87639abf618e2670488f9b1827f
Lucas Ilussencio da Silva <ilussencio@gmail.com>
2023-12-05 15:51:34 -0300

    testes
    testes

--- src/main/kotlin/br/com/srbit/creditapplicationapi/service/impl/CreditService.kt
+++ src/main/kotlin/br/com/srbit/creditapplicationapi/service/impl/CreditService.kt
@@ -19,7 +19,7 @@ class CreditService(
         return this.creditRepository.save(credit)
     }
 
-    override fun findAllByCustomer(customerId: Long): List<Credit> = this.creditRepository.findAllByCredits(customerId)
+    override fun findAllByCustomer(customerId: Long): List<Credit> = this.creditRepository.findAllByCustomerId(customerId)
 
     override fun findByCreditCode(customerId: Long, creditCode: UUID): Credit {
         val credit: Credit = this.creditRepository.findByCreditCode(creditCode)
--- src/test/kotlin/br/com/srbit/creditapplicationapi/CreditApplicationApiApplicationTests.kt
+++ src/test/kotlin/br/com/srbit/creditapplicationapi/CreditApplicationApiApplicationTests.kt
@@ -1,10 +1,13 @@
 package br.com.srbit.creditapplicationapi
 
+import br.com.srbit.creditapplicationapi.repositories.CustomerRepository
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class CreditApplicationApiApplicationTests {
+	@Autowired lateinit var customerRepository: CustomerRepository
 
 	@Test
 	fun contextLoads() {
